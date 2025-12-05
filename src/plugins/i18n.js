2a7f5ed84f170b8fab6f194205956044596a5d06
kotnikd3 <denis.kotnik@gmail.com>
2025-12-05 18:46:15 +0100

    feat: dynamic localization based on URL

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -2,9 +2,14 @@ import { createI18n } from 'vue-i18n';
 import en from '@/locales/en.json';
 import sl from '@/locales/sl.json';
 
+let locale = 'sl' // default
+if (window.location.hostname.endsWith('.com')) {
+    locale = 'en'
+}
+
 const i18n = createI18n({
-	locale: 'en',
-  	fallbackLocale: 'en',
+	locale: locale,
+  	fallbackLocale: 'sl',
   	messages: {
 		en,
 		sl,
